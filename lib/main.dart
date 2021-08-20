f1a9d1c61e92d5956445d67346e157b9b4c4a347
josuegerardo96 <josuegerardo09@gmail.com>
2021-08-20 13:07:12 -0600

    Se agrega media parte del proyecto

--- lib/main.dart
+++ lib/main.dart
@@ -1,105 +1,123 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: main_screen(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class main_screen extends StatefulWidget {
+  @override
+  _main_screenState createState() => _main_screenState();
+}
+
+class _main_screenState extends State<main_screen> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
 
-        primarySwatch: Colors.green,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+
+
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(
+          statusBarColor: Color(0xff1B76FF),
+        )
     );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+
+      body: SafeArea(
+
+        child: Container(
+
+          child: Column(
+
+            children: <Widget>[
+
+              //Primera línea, texto de la app
+              Padding(
+                padding: EdgeInsets.only(top: 15.0, left:15.0),
+                
+                
+                child: Row(
+                  children: <Widget>[
+                    new Icon(
+                      Icons.arrow_back_ios, 
+                      color: Color(0xff202B57), 
+                      size: 18.0),
+              
+                    new Text(
+                      "Tasks",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Color(0xff202B57),
+                      ),
+                    )
+                  ],
+                )
+              ),
+
+              //Second line
+
+              Padding(
+                padding: EdgeInsets.only(top: 40.0, left: 20.0, right: 40.0),
+
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: new Text("Lists", style: TextStyle(fontSize: 14.0, color: Color(0xffA9AECD)))),
+                    new Text("0 lists", style: TextStyle(fontSize: 14.0, color: Color(0xffA9AECD)))
+                  ],
+                ),
+              ),
+
+
+
+              Padding(
+                padding: EdgeInsets.only(top:20.0, left: 10.0, right: 10.0),
+                child: Row(
+                  children: <Widget>[
+
+
+                    // Cuando está vacío entonces pongo esto
+                    Card(
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(5.0),
+                            child: Image(
+                              image: AssetImage('assets/relax2.png'),
+                              width: 128,
+                              height: 128,
+                            ),
+                            )
+                        ],
+                      ),
+                    )
+
+                  ],
+                ),
+              )
+
+
+
+
+
+
+
+
+            ],
+
+
+
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
